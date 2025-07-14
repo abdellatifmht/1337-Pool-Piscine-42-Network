d48d3f1bcce4e9022a17407011b147b8f807858c
Maestro <109997090+abdellatifmht@users.noreply.github.com>
2025-07-14 01:29:56 +0100

    Update ft_ultimate_ft.c
    you have not use this function

--- DAYS/C01/ex01/ft_ultimate_ft.c
+++ DAYS/C01/ex01/ft_ultimate_ft.c
@@ -5,19 +5,6 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    ft_putnbr(int n)
-{
-    if (n < 0)
-    {
-        ft_putchar('-');
-        n = -n;
-    }
-    if (n > 9)
-    {
-        ft_putnbr(n / 10);
-    }
-    ft_putchar((n % 10) + '0');
-}
 
 void    ft_ultimate_ft(int *********nbr)
 {
